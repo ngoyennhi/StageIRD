be6d0682bcd90a56dfe81bf51cb105e30f96d999
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-05-08 07:02:03 +0200

    add vue/formDownload_neige.php and vue/formUpoad_neige.php

---
+++ vue/formDowload_neige.php
@@ -0,0 +1,7 @@
+<div class="container">
+        <div class="row align-items-center justify-content-center">
+            <form action='control/c_formDownload_neige.php' method='post' enctype = 'multipart/form-data' class="d-flex justify-content-center align-items-center">
+                <input class="transbutton noselect align-items-center mb-3" type ='submit' name='saisie[ok]' value = 'Click here to transformer'/>
+            </form>
+        </div>
+</div>
